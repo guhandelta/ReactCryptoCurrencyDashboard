d0b7f9500c4f7a31f67a4b978e01a5e6a83eb90f
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-04-30 11:18:25 -0400

    Made the grid layout responsive, wihtout any media queries, by using auto-fill

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -6,7 +6,13 @@ import CoinTile from './CoinTile'
 
 export const StyledCoindGrid = styled.div`
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+    /*Grid table Responsiveness without @media_queries
+      auto-fill => fils the row with as many columns as it can fit. The new columsn may be empty but they will still occupy a designated-
+    - space in row => Place the columns in the space, even if it has no content in it
+      auto-fit => fits the currently available columns into the space, by exapnding them so that they take up any avaialable space => Place
+    - only the available columns to the full width
+    */
     grid-gap: 20px;
     margin-top: 40px;
 `
